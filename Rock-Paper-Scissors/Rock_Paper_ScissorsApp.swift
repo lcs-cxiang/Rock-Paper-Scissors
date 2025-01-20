73f1668574b0f8571b811f94762681e06e3db42a
Kyle Xiang <cxiang@lcs.on.ca>
2025-01-20 09:42:04 -0500

    Initial Commit

---
+++ Rock-Paper-Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Chujun Xiang on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
