71480184fc7d50fea0ed0e0ec62383837e2bd95a
Kyle Xiang <cxiang@lcs.on.ca>
2025-01-21 12:29:09 -0500

    Still need to save the history and done.

--- Rock-Paper-Scissors/View/GameView.swift
+++ Rock-Paper-Scissors/View/GameView.swift
@@ -14,7 +14,7 @@ struct GameView: View {
     
     var body: some View {
         NavigationStack{
-            VStack(spacing: 30) {
+            VStack(spacing: 30){
                 Text("Choose a chioce between Rock,Paper and Scissors")
                 
                 Text("Make Your Choice!")
@@ -27,81 +27,69 @@ struct GameView: View {
                         Text("Rock")
                     }
                     .buttonStyle(.borderedProminent)
-                    
-                    Text(feedback)
-                        .font(
-                            .custom(
-                                "BradleyHandITCTT-Bold",
-                                size: 24.0,
-                                relativeTo: .title3
-                            )
-                        )
                     Button {
                         Scissors()
                     } label: {
                         Text("Scissors")
                     }
                     .buttonStyle(.borderedProminent)
-                    
-                    Text(feedback)
-                        .font(
-                            .custom(
-                                "BradleyHandITCTT-Bold",
-                                size: 24.0,
-                                relativeTo: .title3
-                            )
-                        )
                     Button {
                         Paper()
                     } label: {
                         Text("Paper")
                     }
                     .buttonStyle(.borderedProminent)
-                    
-                    Text(feedback)
-                        .font(
-                            .custom(
-                                "BradleyHandITCTT-Bold",
-                                size: 24.0,
-                                relativeTo: .title3
-                            )
-                        )
                 }
+                Text(feedback)
+                    .font(
+                        .custom(
+                            "BradleyHandITCTT-Bold",
+                            size: 24.0,
+                            relativeTo: .title3
+                        )
+                    )
+                Button {
+                    reset()
+                } label: {
+                    Text("Reset")
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.red)
+                Spacer()
             }
+            .navigationTitle("Rock Paper Scissors")
         }
-        
-        
-        .navigationTitle("Rock Paper Scissors")
     }
     func Rock() {
         if target == 3{
-            feedback = "You won!"
+            feedback = "You won, I chose Scissors"
         } else if target == 2{
-            feedback = "You lost!"
+            feedback = "You lost, I chose Paper"
         } else {
-            feedback = "Tied!"
+            feedback = "Tied, I chose Rock"
         }
     }
     func Scissors() {
         if target == 2{
-            feedback = "You won!"
+            feedback = "You won,I chose Paper"
         } else if target == 1{
-            feedback = "You lost!"
+            feedback = "You lost, I chose Rock"
         } else {
-            feedback = "Tied!"
+            feedback = "Tied, I chose Scissors"
         }
     }
     func Paper() {
         if target == 1{
-            feedback = "You won!"
+            feedback = "You won, I chose Rock"
         } else if  target == 3{
-            feedback = "You lost!"
+            feedback = "You lost, I chose Scissors"
         } else {
-            feedback = "Tied!"
+            feedback = "Tied, I chose Paper"
         }
     }
     func reset() {
-        
+        feedback = ""
+        target = Int.random(in: 1...3)
     }
 }
 #Preview {
